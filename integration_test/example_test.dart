4ac8a1b8c0a852fb42dfa4a16d1f753ffa8fddb5
Jan <jst@linux.com>
2023-12-05 12:25:46 +0100

    test: setup project for aws device farm tests

--- integration_test/example_test.dart
+++ integration_test/example_test.dart
@@ -2,22 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
 
+import 'package:patrolaws/src/app.dart';
+import 'package:patrolaws/src/settings/settings_controller.dart';
+import 'package:patrolaws/src/settings/settings_service.dart';
+
 void main() {
   patrolTest(
-    'counter state is the same after going to home and switching apps',
+    'list view to detail view and back',
     ($) async {
-      // Replace later with your app's main widget
-      await $.pumpWidgetAndSettle(
-        MaterialApp(
-          home: Scaffold(
-            appBar: AppBar(title: const Text('app')),
-            backgroundColor: Colors.blue,
-          ),
-        ),
-      );
+      final WidgetTester tester = $.tester;
 
-      expect($('app'), findsOneWidget);
-      await $.native.pressHome();
+      final settingsController = SettingsController(SettingsService());
+      await settingsController.loadSettings();
+
+      await $.pumpWidgetAndSettle(MyApp(settingsController: settingsController));
+
+      expect($('Sample Items'), findsOneWidget);
+      expect($('SampleItem 1'), findsOneWidget);
+      expect($('SampleItem 2'), findsOneWidget);
+      expect($(ListView).$(ListTile).$('SampleItem 3'), findsOneWidget);
+
+      await $(ListView).$(ListTile).$('SampleItem 2').tap();
+      expect($('Item Details'), findsOneWidget);
+      expect($('More Information Here'), findsOneWidget);
+
+      await $(find.byTooltip('Back')).tap();
+      expect($('Sample Items'), findsOneWidget);
     },
   );
 }
